10646ac7e1df15dea280417c7743382df3a50cd1
MDINVILLE <matteo.dinville@ynov.com>
2021-01-25 14:22:40 +0100

    ascii

--- ascii/ascii-art.go
+++ ascii/ascii-art.go
@@ -14,64 +14,55 @@ func Ascii(txt, namefile string) string {
 	tab := SplitTab(FindTab(arg))
 
 	// tab a 2 entrées
-	for _, l := range tab { //execute printletter pour chaque ligne de mon tab
+	for _, l := range tab {
 		convert += PrintLetterAscii(file2, l)
 	}
 	return convert
 }
 
-//permet d'afficher les lettres ascii à partir de la ligne calculé
+//permet d'afficher les lettres ascii à partir de la ligne que nous souhaitons
 func PrintLetterAscii(file2 []string, tab []int) string {
 	convert := ""
 	for i := -1; i < 7; i++ {
 		for _, l := range tab {
 			convert += string(file2[l+i])
-			// fmt.Printf(file2[l+i]) //afficher tout les lignes de la lettre
 		}
 		convert += "\n"
-		// fmt.Printf("\n")
 	}
 	return convert
 }
 
 //permet de calculer la ligne du début de la lettre
-////permet de calculer la ligne du commencement de l'ascci-art à partir de la valeur ascii du caractère - '\n'
-// prend la valeur 0 dans ce tableau
-
-//c'est pour remplir ton tab avec
-//les valeurs des premières lignes des caractères que tu veu afficher
 func FindTab(arg []byte) []int {
 	var tab []int
-	tab = make([]int, len(arg)) // créer un tab vide de la taille voulu ici arg
-	for i, l := range arg {     // meme pour tab
+	tab = make([]int, len(arg))
+	for i, l := range arg {
 		if l == '\n' {
 			tab[i] = 0
 		} else {
-			tab[i] = int(l-' ')*9 + 1 // determine la 1ere ligne de la lettre
+			tab[i] = int(l-' ')*9 + 1
 		}
 	}
 	return tab
 }
 
 //tableau divisé en 2 entrées, pour toutes les valeurs 0
-//alors c'est juste par rapport au sol de ligne. En gros si j'ai "hello\nyou"
-//il faut d'abord affiché "hello" donc la première ligne du tableau et ensuite faire le "you"
 
 func SplitTab(tab []int) [][]int {
 	var board [][]int
 	count := 1
 	n := 0
-	for _, l := range tab { //compte le nombre de 0 (\n)
+	for _, l := range tab {
 		if l == 0 {
 			count++
 		}
 	}
 	board = make([][]int, count)
 	for _, l := range tab {
-		if l == 0 { // qd changer de ligne
+		if l == 0 { //
 			n++
 		} else {
-			board[n] = append(board[n], l) // rajouter des caracteres dans la ligne carac 1 apres l'autre
+			board[n] = append(board[n], l)
 		}
 	}
 	return board
